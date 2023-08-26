02b8ec24ddd6c37ed0ee7d616269c1255e460086
Joel Orellana <joelorellana989@gmail.com>
2023-08-26 16:14:42 -0600

    Version completa del dia 39: Envio de un SMS si se detecta menor precio que un valor por defecto del excel sheet

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+import os
 from sheety import Sheet
 from sms import SMS
 from tequila import Tequila
+from dates import get_tomorrow, get_six_months_later
+from dotenv import load_dotenv
 
+load_dotenv(override=True)
 
+MY_NUMBER = os.getenv("MY_NUMBER")
+
+# creating objects for project
 msg_sender = SMS()
 my_sheet = Sheet()
 my_tequila = Tequila()
+
+# READING FROM SHEET
 
 content = my_sheet.read_sheet()  # first read sheet
 
@@ -17,4 +26,16 @@ for el in content:  # iterate through content to see if there are blank iata cod
     else:
         continue
 
-# check prices of flights
+# CHECKING PRICES OF FLIGHTS
+for el in content:
+    iata = el['iataCode']
+    saved_price = el['lowestPrice']
+    tomorrow = get_tomorrow()
+    six_months_later = get_six_months_later()
+    actual_price = my_tequila.search_flights('LAX', iata, tomorrow, six_months_later)
+    if actual_price is None:
+        continue
+    elif actual_price < saved_price:
+        msg_sender.send_msg(MY_NUMBER, f"Lowest price found: ${actual_price}")
+    else:
+        print("No new price found", actual_price, saved_price)
